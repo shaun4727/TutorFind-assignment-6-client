cb6864d48e91628c326c2f2eb869267d13d12896
Shaun Hossain <107396795+shaun4727@users.noreply.github.com>
2025-03-13 21:31:20 +0600

    suspense added

--- src/app/tutors/page.tsx
+++ src/app/tutors/page.tsx
@@ -1,10 +1,13 @@
 import '@/../../assets/root.css';
 import TutorsParent from '@/Components/tutors/components/TutorParentComp';
+import { Suspense } from 'react';
 
 export default function Tutors() {
   return (
     <div className="default-padding-body">
-      <TutorsParent />
+      <Suspense fallback={<div>Loading payment details...</div>}>
+        <TutorsParent />
+      </Suspense>
     </div>
   );
 }
